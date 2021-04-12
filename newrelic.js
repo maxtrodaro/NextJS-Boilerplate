099141d2c3e7acf2f3909ef1816d05edb906fdd8
Eduardo Godoy's <dudurossigodoy2010@gmail.com>
2021-04-12 11:53:57 -0300

    GB files

---
+++ newrelic.js
@@ -0,0 +1,33 @@
+'use strict'
+exports.config = {
+  agent_enabled: process.env.NODE_ENV === 'homologation' || process.env.NODE_ENV === 'production',
+  app_name: ['$(NEW_RELIC_APP_NAME)'],
+  license_key: '$(NEW_RELIC_LICENSE_KEY)',
+  distributed_tracing: {
+    enabled: true
+  },
+  logging: {
+    level: 'info',
+    enabled: true
+  },
+  error_collector: {
+    enabled: true,
+    ignore_status_codes: [301, 302, 404],
+    ignore_classes: ['MulterError']
+  },
+  allow_all_headers: true,
+  attributes: {
+    exclude: [
+      'request.headers.cookie',
+      'request.headers.authorization',
+      'request.headers.proxyAuthorization',
+      'request.headers.setCookie*',
+      'request.headers.x*',
+      'response.headers.cookie',
+      'response.headers.authorization',
+      'response.headers.proxyAuthorization',
+      'response.headers.setCookie*',
+      'response.headers.x*'
+    ]
+  }
+}
